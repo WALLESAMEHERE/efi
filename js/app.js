9524b8b45109f4a7019b43f0023eb14dab0a5c10
Luke <Luke>
2016-08-11 19:49:04 +0200

    ajax request / login / api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 $(document).foundation();
+
+$(document).ready(function(){
+	$('#user_btn').on('click', function(){
+		 var pass = $('#password').val();
+		 	
+	var text = {
+		login:"efi",
+		password:pass
+	};
+	sendAjax(text);
+	});
+	 function sendAjax(text){
+		$.ajax({
+		  type: "post",
+		  data:text,
+		  url: "https://efigence-camp.herokuapp.com/api/login",
+		  error: function(response) {
+		  	var obj = response.responseText
+		    document.getElementById("blad").innerHTML = obj.message;
+		  },
+		  success: function(response) {
+		    console.log(response);
+		  }
+		});
+	}
+});
+
+
+//http://stackoverflow.com/questions/16781357/get-element-of-a-responsetext-json-of-an-openlayers-get-request
