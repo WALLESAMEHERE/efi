9957e9fa9ddb1fd945534cfa3600a30d80fd4195
WALLE <hulk13@op.pl>
2016-08-15 11:49:43 +0200

    validation form / hover effects / replace to dashboard

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
 $(document).foundation();
 
+/* ON CLICK !!!!!!
+
 $(document).ready(function(){
-	$('#user_btn').on('click', function(){
-		 var pass = $('#password').val();
-		 	
-	var text = {
-		login:"efi",
-		password:pass
-	};
-	sendAjax(text);
+	 	
+	$('#user_btn').on('click', function(e){
+		e.preventDefault();
+		$(this).attr('disabled', 'disabled'); // disabled submit button
+		 setTimeout(function() {
+	        $('#user_btn').attr("disabled", false); // enabled submit button
+	    }, 4000);
+
+		var pass = $('#password').val(); // input password
+		var user = $('#user').text();	// user name
+		var text = {
+			login:user,
+			password:pass
+		};	
+		// validation
+		if(pass == ""){
+			$('#blad')  // show error 
+			.fadeIn('slow')
+			.html('dupa haslo')
+			.delay(3000)
+			.fadeOut('slow');
+			return false;
+		} 
+		else{
+			$("#blad").html (''); // clear error
+			sendAjax(text); // arrow to server
+		}		
 	});
 	 function sendAjax(text){
 		$.ajax({
@@ -16,15 +37,73 @@ $(document).ready(function(){
 		  data:text,
 		  url: "https://efigence-camp.herokuapp.com/api/login",
 		  error: function(response) {
-		  	var obj = response.responseText
-		    document.getElementById("blad").innerHTML = obj.message;
+		  	var obj = JSON.parse(response.responseText);
+		    $('#blad') // show error
+		    .fadeIn('slow')
+		    .html(obj.message)
+		    .delay(3000)
+		    .fadeOut('slow');
+		    return false;
+		    
 		  },
 		  success: function(response) {
-		    console.log(response);
+		  	$("#blad").html(""); // clear error
+		    $(window).attr("location","http://google.pl"); // replace to dashboard
 		  }
 		});
 	}
 });
 
+*/
+// ON SUBMIT !!!!!!
 
-//http://stackoverflow.com/questions/16781357/get-element-of-a-responsetext-json-of-an-openlayers-get-request
+$(document).ready(function(){
+	$('#login').on('submit', function(e){
+		e.preventDefault();
+		$(this).attr('disabled', 'disabled'); // disabled submit button
+		 setTimeout(function() {
+	        $('#user_btn').attr("disabled", false); // enabled submit button
+	    }, 4000);
+		var pass = $('#password').val(); // input password
+		var user = $('#user').text();	// user name
+		var text = {
+			login:user,
+			password:pass
+		};	
+		// validation
+		if(pass == ""){
+			$('#blad')  // show error 
+			.fadeIn('slow')
+			.html('dupa haslo')
+			.delay(3000)
+			.fadeOut('slow');
+			return false;
+		} 
+		else{
+
+			$("#blad").html(''); // clear error
+			sendAjax(text); // arrow to server 
+			return false;
+		}
+	});
+	function sendAjax(text){
+		$.ajax({
+		  type: "post",
+		  data:text,
+		  url: "https://efigence-camp.herokuapp.com/api/login",
+		  error: function(response) {
+		  	var obj = JSON.parse(response.responseText);
+		    $('#blad') // show error
+		    .fadeIn('slow')
+		    .html(obj.message)
+		    .delay(3000)
+		    .fadeOut('slow');
+		    return false;	    
+		  },
+		  success: function(response) {
+		  	$("#blad").html(""); // clear error
+		    $(window).attr("location","http://google.pl"); // replace to dashboard
+		  }
+		});
+	}
+});
